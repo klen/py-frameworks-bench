70e01627f0a289e6b6f8b00ac46cd990ecaf422b
Kirill Klenov <horneds@gmail.com>
2015-05-07 16:31:31 +0300

    update scripts

--- deploy/roles/bench.setup/files/cvs-report.lua
+++ deploy/roles/bench.setup/files/cvs-report.lua
@@ -10,7 +10,7 @@ done = function(summary, latency, requests)
         file.write(
             file,
             string.format(
-                "%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d,%d\n",
+                "%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d,%d\n",
                 latency.min / 1000,
                 latency:percentile(50) / 1000,
                 latency:percentile(75) / 1000,
@@ -18,7 +18,8 @@ done = function(summary, latency, requests)
                 latency:percentile(99) / 1000,
                 latency:percentile(99.9) / 1000,
                 latency.max / 1000,
-                summary.duration / 1000,
+                latency.mean / 1000,
+                summary.duration / 1000000,
                 summary.requests,
                 summary.errors.status));
         file.close()
--- deploy/roles/bench.setup/templates/run_tests.py
+++ deploy/roles/bench.setup/templates/run_tests.py
@@ -11,16 +11,24 @@ frameworks = [
 ]
 
 root = "/var/www"
-data = {}
+cdata = {}
+tdata = {}, {}, {}
 
 for framework in frameworks:
     path = "%s/%s/%s" % (root, framework, 'test.sh')
     subprocess.check_call(path)
     with open('%s.csv' % framework, 'r') as f:
         results = []
+        marker = 0
         for line in f.readlines():
             line = line.strip()
-            results.append([float(c) for c in line.split(',')])
-        data[framework] = results
+            nums = [float(c) for c in line.split(',')]
+            _, p50, p75, _, _, _, _, avg, dur, req, err = nums
+            tdata[marker][framework] = p50, p75, avg, req / dur, err / req
+            marker += 1
+            results.append(nums)
+        cdata[framework] = results
 
-sys.stdout.write(json.dumps(data))
+for data in tdata:
+    sys.stdout.write('\n' + json.dumps(data) + '\n')
+sys.stdout.write('\n' + json.dumps(cdata) + '\n')
