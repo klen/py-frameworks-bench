26271ec63e6a41f693ddc3a92e515461833900d1
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-01-21 16:02:37 -0800

    add Xpresso to frameworks

---
+++ frameworks/xpresso/app.py
@@ -0,0 +1,74 @@
+import time
+from dataclasses import dataclass
+from typing import List
+from uuid import uuid4
+from xpresso import App, FromHeader, FromMultipart, FromPath, Request, Path, ExtractField, FromFile, UploadFile, Dependant
+from starlette.routing import BaseRoute
+from starlette.responses import HTMLResponse, JSONResponse, PlainTextResponse, Response
+
+
+routes: List[BaseRoute] = []
+
+
+# first add ten more routes to load routing system
+# ------------------------------------------------
+async def req_ok() -> HTMLResponse:
+    return HTMLResponse('ok')
+
+
+async def req_ok_dyn(part: FromPath[str]) -> HTMLResponse:
+    return HTMLResponse('ok')
+
+
+for n in range(5):
+    routes.append(Path(f"/route-{n}", get=req_ok))
+    routes.append(Path(f"/route-dyn-{n}", get=req_ok_dyn))
+
+
+# then prepare endpoints for the benchmark
+# ----------------------------------------
+async def html() -> HTMLResponse:
+    """Return HTML content and a custom header."""
+    content = "<b>HTML OK</b>"
+    headers = {'x-time': f"{time.time()}"}
+    return HTMLResponse(content, headers=headers)
+
+
+routes.append(Path("/html", get=html))
+
+@dataclass
+class MultiPartForm:
+    file: ExtractField[FromFile[UploadFile]]
+
+
+async def upload(form: FromMultipart[MultiPartForm]) -> Response:
+    """Load multipart data and store it as a file."""
+    with open(f"/tmp/{uuid4().hex}", 'wb') as target:
+        target.write(await form.file.read())
+
+    return PlainTextResponse(target.name)
+
+
+routes.append(Path("/upload", post=upload))
+
+
+def check_auth_header(authorization: FromHeader[str]) -> None:
+    ...
+
+
+async def api(request: Request, user: FromPath[int], record: FromPath[str], ):
+    """Check headers for authorization, load JSON/query data and return as JSON."""
+    if request.headers.get('authorization') is None:
+        return Response('ERROR', status_code=401)
+
+    return JSONResponse({
+        'params': {'user': user, 'record': record},
+        'query': dict(request.query_params),
+        'data': await request.json(),
+    })
+
+
+routes.append(Path("/api/users/{user}/records/{record}", put=api, dependencies=[Dependant(check_auth_header)]))
+
+
+app = App(routes=routes)
