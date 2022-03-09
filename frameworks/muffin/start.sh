ca5009de946a1c5b8845fa16408328783bf81ee5
Kirill Klenov <horneds@gmail.com>
2022-03-09 18:20:57 +0300

    feat: update frameworks

--- frameworks/muffin/start.sh
+++ frameworks/muffin/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/bin/gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 app:app
+gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 app:app
--- frameworks/sanic/start.sh
+++ frameworks/sanic/start.sh
@@ -4,5 +4,4 @@
 sanic --host 0.0.0.0 --port=8080 --workers=1 app:app
 
 # /usr/local/bin/gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 app:app
-
 # /usr/local/bin/gunicorn -k sanic.worker.GunicornWorker -b 0.0.0.0:8080 app:app
