b659b300914f7ddf9bf28e6a9fa986a72e4f40fd
Dominik Gasparic <56818232+codedoga@users.noreply.github.com>
2022-10-13 12:25:14 +0200

    opt for django

--- frameworks/django/app.py
+++ frameworks/django/app.py
@@ -1,12 +1,10 @@
 from django.core.asgi import get_asgi_application
 from django.conf import settings
 
-from . import views
-
 settings.configure(
     SECRET_KEY='nosecret',
     DEBUG=False,
-    ROOT_URLCONF = views,
+    ROOT_URLCONF="views",
 )
 
 app = get_asgi_application()
